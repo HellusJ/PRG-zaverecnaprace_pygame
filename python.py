1925c45b4d555d780dd53f7b6623224eb84ec98b
HellusJ <Hel42252@spseol.cz>
2024-05-19 20:20:06 +0200

    mam menu a pravidla a ted uz zbyva jenom hra a taky jsem udelal mega najetou funkci

--- python.py
+++ python.py
@@ -9,6 +9,7 @@ pygame.display.set_caption("Kostky")
     #OBRAZKY
 #barvy
 menucolor = (220,255,255)
+textcolor = (0,0,0)
 
 #pozadi
 pozadi = pygame.image.load("background.png")
@@ -16,11 +17,19 @@ pozadi = pygame.image.load("background.png")
 #buttons
 playbutton = pygame.transform.scale(pygame.image.load("button_play.png"), (300,150))
 playbutton_rect = playbutton.get_rect()
-playbutton_rect.center = (355, 245)
+playbutton_rect.center = (355, 160)
 
 quitbutton = pygame.transform.scale(pygame.image.load("button_quit.png"), (260,140))
 quitbutton_rect = playbutton.get_rect()
-quitbutton_rect.center = (369, 420)
+quitbutton_rect.center = (369, 335)
+
+rulesbutton = pygame.transform.scale(pygame.image.load("rules_button.png"), (355,140))
+rulesbutton_rect = playbutton.get_rect()
+rulesbutton_rect.center = (317, 500)
+
+stepback = pygame.transform.scale(pygame.image.load("stepback.png"), (45,45))
+stepback_rect = playbutton.get_rect()
+stepback_rect.topleft = (5, 5)
 
 #ruce
 ruce = pygame.transform.scale(pygame.image.load("ruce.png"), (280, 120))
@@ -41,9 +50,20 @@ kostka4 = pygame.image.load("kostky_4.png")
 kostka5 = pygame.image.load("kostky_5.png")
 kostka6 = pygame.image.load("kostky_6.png")
 
+#pravidla
+rules1 = pygame.transform.scale(pygame.image.load("rules_picture.png"), (560, 560))
+rules1_rect = rules1.get_rect()
+rules1_rect.topleft = (70, 15)
+
 animace_ruce = [ruce, ruce_shaking_1,ruce_Shaking_2, ruce_throw]
 kostky = [kostka1,kostka2,kostka3,kostka4,kostka5,kostka6]
 
+#text
+def napsattext(text,font_size,x,y):
+    vykres = pygame.font.SysFont(None, font_size).render(text, True, textcolor)
+    screen.blit(vykres, (x,y))
+
+#menu
 def menu():
     while True:
         for event in pygame.event.get():
@@ -54,25 +74,43 @@ def menu():
                     hra()
                 elif quitbutton_rect.collidepoint(event.pos):
                     sys.exit()
+                elif rulesbutton_rect.collidepoint(event.pos):
+                    rules()
                 
         screen.fill(menucolor)
         screen.blit(playbutton, playbutton_rect)
         screen.blit(quitbutton, quitbutton_rect)
+        screen.blit(rulesbutton, rulesbutton_rect)
         pygame.display.flip()
 
+#pravidla
+def rules():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if stepback_rect.collidepoint(event.pos):
+                    menu()
+
+        screen.fill(menucolor)
+        screen.blit(stepback, stepback_rect)
+        screen.blit(rules1, rules1_rect)
+        napsattext("První kdo získá 10000 bodů tak vyhrává!", 50, 20,600)
+
+        pygame.display.flip()
+        pygame.time.delay(60)
+
+#HRA
 def hra():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        #pozadi
+        
         screen.blit(pozadi, (0, 0))
-
-        #ruce
         screen.blit(ruce, (210,580))
-
         screen.blit(ruce_opp, (210, 0))
-
         pygame.display.flip()
         pygame.time.delay(60)
         
